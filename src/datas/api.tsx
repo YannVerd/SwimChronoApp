11b3e04e79526769df37ec82db0bf5dc48910b9d
YannV <yann.e.verdier@gmail.com>
2024-10-24 08:38:49 +0000

    essential package and function from other project

---
+++ src/datas/api.tsx
@@ -0,0 +1,41 @@
+
+
+export const endpoints = {
+    login: "api/user/login",
+    register: "api/user/regisrer",
+    profil: "api/user/profil/{id}",
+    helloworld: "api/",
+}
+
+
+/**
+ * Function that centralizes fetch requests.
+    The request method is detected automatically
+    She works with the above endpoints.
+ * @param endpoint property of above endpoints
+ * @param datas object for method POST body
+ * @returns result of request (will probably change to update the context)
+ */
+export  function requestAPI(endpoint: string, datas?: Object, id?: string){
+    let url = `http://localhost:3006/${endpoint}`;
+    let options: RequestInit = {
+        method: "GET",
+        headers: {
+            "Content-Type": "application/json",
+          },
+    }
+    
+    if(id){
+        url.replace('{id}', id);
+    }
+
+    if(datas){
+        options.method = 'POST';
+        options.body = JSON.stringify(datas);
+    }   
+    
+    return fetch(url, options)
+}
+        
+        
+    
