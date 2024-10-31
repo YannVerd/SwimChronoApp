1db1c53539cff0b7775da56231401b2b0f4ebfd1
YannV <yann.e.verdier@gmail.com>
2024-10-31 10:49:23 +0000

    fix navbar

--- src/components/fixed/NavBar.tsx
+++ src/components/fixed/NavBar.tsx
@@ -1,5 +1,6 @@
 import { useNavigate } from "react-router-dom";
-import { AppBar, Box, Button, Toolbar, Typography, IconButton } from "@mui/material";
+import { AppBar, Box, Button, Toolbar, IconButton } from "@mui/material";
+import Title from "../common/CustomTypo";
 
 const NavBar = () => {
   const navItems = [{name: 'Home', link:'/dashboard'}, {name:'About', link:'/about'},{name:'Contact', link:'/contact'}];
@@ -8,15 +9,9 @@ const NavBar = () => {
     <AppBar component="nav">
         <Toolbar>
           <IconButton edge="start" color="inherit" onClick={() => navigate("/")}>
-            <img src="/logo-hackathon.png" alt="Logo" style={{ width: '4rem', height: '4rem' }} />
+            <img src="#" alt="Logo" style={{ width: '4rem', height: '4rem' }} />
           </IconButton>
-          <Typography
-            variant="h6"
-            component="div"
-            sx={{ flexGrow: 1, display: { xs: 'none', sm: 'block' } }}
-          >
-            CodeBusters
-          </Typography>
+          <Title />
           <Box sx={{ display: { xs: 'none', sm: 'block' } }}>
             {navItems.map((item) => (
               <Button key={item.name} sx={{ color: '#fff' }} onClick={()=>{navigate(`${item.link}`)}}>
